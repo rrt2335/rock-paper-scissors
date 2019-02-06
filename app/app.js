1be081dc680f5c049ab91d050961430f9f7b5885
Ryan Thomas <rrt2335@gmail.com>
2019-02-06 16:49:06 -0700

    Game logic

--- app/app.js
+++ app/app.js
@@ -7,23 +7,42 @@ let choices = [
 ]
 
 function pickRock() {
-    document.querySelector('#title').innerHTML = `<h1>You have chosen rock!</h1>`
+
     let playerChoice = choices[0];
-    return playerChoice;
-   
+    document.querySelector('#title').innerHTML = `<h1>You have chosen rock!</h1>`
+
     //invoke setComputerChoice() and save the return value to a computerChoice variable
-    document.querySelector('#result').innerHTML = `<h3>The computer has chosen rock!</h3>`
+    let computerChoice = setComputerChoice();
+    document.querySelector('#result').innerHTML = `<h3>The computer has chosen ${computerChoice}! You ${compareChoices(playerChoice, computerChoice)}!</h3>`
+
     //invoke a play/compare function and pass your choice 'rock' and the computerChoice
     //eg: play('rock', computerChoice)
-    compareChoices();
+    
 }
 
 function pickPaper() {
+
+    let playerChoice = choices[1];
     document.querySelector('#title').innerHTML = `<h1>You have chosen paper!</h1>`
+
+    //invoke setComputerChoice() and save the return value to a computerChoice variable
+    let computerChoice = setComputerChoice();
+    document.querySelector('#result').innerHTML = `<h3>The computer has chosen ${computerChoice}! You ${compareChoices(playerChoice, computerChoice)}!</h3>`
+
+    //invoke a play/compare function and pass your choice 'rock' and the computerChoice
+    //eg: play('rock', computerChoice)
 }
 
 function pickScissors() {
+    let playerChoice = choices[2];
     document.querySelector('#title').innerHTML = `<h1>You have chosen scissors!</h1>`
+
+    //invoke setComputerChoice() and save the return value to a computerChoice variable
+    let computerChoice = setComputerChoice();
+    document.querySelector('#result').innerHTML = `<h3>The computer has chosen ${computerChoice}! You ${compareChoices(playerChoice, computerChoice)}!</h3>`
+
+    //invoke a play/compare function and pass your choice 'rock' and the computerChoice
+    //eg: play('rock', computerChoice)
 }
 
 function setComputerChoice() {
@@ -34,12 +53,14 @@ function setComputerChoice() {
     return computerChoice;
 }
 
-function compareChoices() {
-    if (playerChoice == computerChoice){
-        console.log("Draw");
+function compareChoices(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {
+        return "tie"
+    } else if (playerChoice < computerChoice && playerChoice != 2) {
+        return "lose"
+    } else {
+        return "win"
     }
 }
 
-console.log("Player choice:" +playerChoice);
-console.log("Player choice:" +computerChoice);
 console.log("Finish!");
