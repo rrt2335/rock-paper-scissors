af3ec6b916d1893a53c5b4ad9a205b2a120b1da6
Ryan Thomas <rrt2335@gmail.com>
2019-02-07 13:08:32 -0700

    Scoring system

--- app/app.js
+++ app/app.js
@@ -11,17 +11,21 @@ let choices = [
 
 let winCount = 0;
 let lossCount = 0;
+let drawCount = 0;
+
 let computerChoice = ''
 let playerChoice = ''
 
 function pickRock() {
 
     playerChoice = choices[0];
-    document.querySelector('#title').innerHTML = `<h1>YOU HAVE CHOSEN ROCK...</h1>`
+    document.querySelector('#title').innerHTML = `<h1>YOU HAVE CHOSEN ROCK.</h1>`
 
     //invoke setComputerChoice() and save the return value to a computerChoice variable
     computerChoice = setComputerChoice();
     document.querySelector('#result').innerHTML = `<h3>THE COMPUTER HAS CHOSEN ${computerChoice}!<br>${compareChoices(playerChoice, computerChoice)}</h3>`
+
+    updateScore();
 
     //invoke a play/compare function and pass your choice 'rock' and the computerChoice
     //eg: play('rock', computerChoice)
@@ -30,11 +34,13 @@ function pickRock() {
 function pickPaper() {
 
     playerChoice = choices[1];
-    document.querySelector('#title').innerHTML = `<h1>YOU HAVE CHOSEN PAPER...</h1>`
+    document.querySelector('#title').innerHTML = `<h1>YOU HAVE CHOSEN PAPER.</h1>`
 
     //invoke setComputerChoice() and save the return value to a computerChoice variable
     computerChoice = setComputerChoice();
     document.querySelector('#result').innerHTML = `<h3>THE COMPUTER HAS CHOSEN ${computerChoice}!<br>${compareChoices(playerChoice, computerChoice)}</h3>`
+
+    updateScore();
 
     //invoke a play/compare function and pass your choice 'rock' and the computerChoice
     //eg: play('rock', computerChoice)
@@ -42,12 +48,13 @@ function pickPaper() {
 
 function pickScissors() {
     playerChoice = choices[2];
-    document.querySelector('#title').innerHTML = `<h1>YOU HAVE CHOSEN SCISSORS...</h1>`
+    document.querySelector('#title').innerHTML = `<h1>YOU HAVE CHOSEN SCISSORS.</h1>`
 
     //invoke setComputerChoice() and save the return value to a computerChoice variable
     computerChoice = setComputerChoice();
     document.querySelector('#result').innerHTML = `<h3>THE COMPUTER HAS CHOSEN ${computerChoice}!<br>${compareChoices(playerChoice, computerChoice)}</h3>`
 
+    updateScore();
     //invoke a play/compare function and pass your choice 'rock' and the computerChoice
     //eg: play('rock', computerChoice)
 }
@@ -62,20 +69,34 @@ function setComputerChoice() {
 
 function compareChoices(playerChoice, computerChoice) {
     if (playerChoice == computerChoice) {
+        drawCount++;
         return "IT'S A DRAW.";
     } else if (playerChoice == "ROCK" && computerChoice == "SCISSORS") {
+        winCount++;
         return "YOU WIN!";
     } else if (playerChoice == "ROCK" && computerChoice == "PAPER") {
+        lossCount++;
         return "YOU LOSE...";
     } else if (playerChoice == "PAPER" && computerChoice == "ROCK") {
+        winCount++;
         return "YOU WIN!";
     } else if (playerChoice == "PAPER" && computerChoice == "SCISSORS") {
+        lossCount++;
         return "YOU LOSE...";
     } else if (playerChoice == "SCISSORS" && computerChoice == "PAPER") {
+        winCount++;
         return "YOU WIN!";
     } else if (playerChoice == "SCISSORS" && computerChoice == "ROCK") {
+        lossCount++;
         return "YOU LOSE...";
     }
 }
+
+function updateScore() {
+    document.querySelector('#win').innerHTML = `<h5>WINS: ${winCount}</h5>`;
+    document.querySelector('#lose').innerHTML = `<h5>LOSSES: ${lossCount}</h5>`
+    document.querySelector('#tie').innerHTML = `<h5>DRAWS: ${drawCount}</h5>`
+}
+
 
 console.log("Finish!");
