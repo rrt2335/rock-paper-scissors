050d71f52453a7fdf65947c3ec259b4a79d0bbba
Ryan Thomas <rrt2335@gmail.com>
2019-03-21 20:40:10 -0600

    Project build

--- app/app.js
+++ app/app.js
@@ -3,11 +3,10 @@ console.log("Start!");
 let choices = [
     "ROCK",
     "PAPER",
-    "SCISSORS"
+    "SCISSORS",
+    "LIZARD",
+    "SPOCK"
 ]
-//rock beats scissors [0] [2]
-//paper beats rock [1] [0]
-//scissors beats paper [2] [1]
 
 let winCount = 0;
 let lossCount = 0;
@@ -19,41 +18,59 @@ let playerChoice = ''
 function pickRock() {
     playSound();
     playerChoice = choices[0];
-    document.querySelector('#title').innerHTML = `<h1>YOU HAVE CHOSEN ROCK.</h1>`
+    document.querySelector('#title').innerHTML = `<h2>YOU HAVE CHOSEN ROCK.</h2>`
 
     //invoke setComputerChoice() and save the return value to a computerChoice variable
     computerChoice = setComputerChoice();
-    document.querySelector('#result').innerHTML = `<h3>THE COMPUTER HAS CHOSEN ${computerChoice}!<br>${compareChoices(playerChoice, computerChoice)}</h3>`
+    document.querySelector('#result').innerHTML = `<h4>THE COMPUTER HAS CHOSEN ${computerChoice}!<br>${compareChoices(playerChoice, computerChoice)}</h4>`
 
     updateScore();
-
-    //invoke a play/compare function and pass your choice 'rock' and the computerChoice
-    //eg: play('rock', computerChoice)
 }
 
 function pickPaper() {
     playSound();
     playerChoice = choices[1];
-    document.querySelector('#title').innerHTML = `<h1>YOU HAVE CHOSEN PAPER.</h1>`
+    document.querySelector('#title').innerHTML = `<h2>YOU HAVE CHOSEN PAPER.</h2>`
 
     //invoke setComputerChoice() and save the return value to a computerChoice variable
     computerChoice = setComputerChoice();
-    document.querySelector('#result').innerHTML = `<h3>THE COMPUTER HAS CHOSEN ${computerChoice}!<br>${compareChoices(playerChoice, computerChoice)}</h3>`
+    document.querySelector('#result').innerHTML = `<h4>THE COMPUTER HAS CHOSEN ${computerChoice}!<br>${compareChoices(playerChoice, computerChoice)}</h4>`
 
     updateScore();
-
-    //invoke a play/compare function and pass your choice 'rock' and the computerChoice
-    //eg: play('rock', computerChoice)
 }
 
 function pickScissors() {
     playSound();
     playerChoice = choices[2];
-    document.querySelector('#title').innerHTML = `<h1>YOU HAVE CHOSEN SCISSORS.</h1>`
+    document.querySelector('#title').innerHTML = `<h2>YOU HAVE CHOSEN SCISSORS.</h2>`
 
     //invoke setComputerChoice() and save the return value to a computerChoice variable
     computerChoice = setComputerChoice();
-    document.querySelector('#result').innerHTML = `<h3>THE COMPUTER HAS CHOSEN ${computerChoice}!<br>${compareChoices(playerChoice, computerChoice)}</h3>`
+    document.querySelector('#result').innerHTML = `<h4>THE COMPUTER HAS CHOSEN ${computerChoice}!<br>${compareChoices(playerChoice, computerChoice)}</h4>`
+
+    updateScore()
+}
+
+function pickLizard() {
+    playSound();
+    playerChoice = choices[3];
+    document.querySelector('#title').innerHTML = `<h2>YOU HAVE CHOSEN LIZARD.</h2>`
+
+    //invoke setComputerChoice() and save the return value to a computerChoice variable
+    computerChoice = setComputerChoice();
+    document.querySelector('#result').innerHTML = `<h4>THE COMPUTER HAS CHOSEN ${computerChoice}!<br>${compareChoices(playerChoice, computerChoice)}</h4>`
+
+    updateScore()
+}
+
+function pickSpock() {
+    playSound();
+    playerChoice = choices[4];
+    document.querySelector('#title').innerHTML = `<h2>YOU HAVE CHOSEN SPOCK.</h2>`
+
+    //invoke setComputerChoice() and save the return value to a computerChoice variable
+    computerChoice = setComputerChoice();
+    document.querySelector('#result').innerHTML = `<h4>THE COMPUTER HAS CHOSEN ${computerChoice}!<br>${compareChoices(playerChoice, computerChoice)}</h4>`
 
     updateScore();
     //invoke a play/compare function and pass your choice 'rock' and the computerChoice
@@ -61,7 +78,7 @@ function pickScissors() {
 }
 
 function setComputerChoice() {
-    // Select a random rock/paper/scissor choice
+    // Select a random rock/paper/scissor/lizard/spock choice
     // let choices = Object.keys(choices)
     let choiceIndex = Math.floor(Math.random() * choices.length)
     computerChoice = choices[choiceIndex]
@@ -78,18 +95,60 @@ function compareChoices(playerChoice, computerChoice) {
     } else if (playerChoice == "ROCK" && computerChoice == "PAPER") {
         lossCount++;
         return "YOU LOSE...";
+    } else if (playerChoice == "ROCK" && computerChoice == "LIZARD") {
+        winCount++;
+        return "YOU WIN!";
+    } else if (playerChoice == "ROCK" && computerChoice == "SPOCK") {
+        lossCount++;
+        return "YOU LOSE...";
     } else if (playerChoice == "PAPER" && computerChoice == "ROCK") {
         winCount++;
         return "YOU WIN!";
     } else if (playerChoice == "PAPER" && computerChoice == "SCISSORS") {
         lossCount++;
         return "YOU LOSE...";
+    } else if (playerChoice == "PAPER" && computerChoice == "LIZARD") {
+        lossCount++;
+        return "YOU LOSE...";
+    } else if (playerChoice == "PAPER" && computerChoice == "SPOCK") {
+        winCount++;
+        return "YOU WIN!";
     } else if (playerChoice == "SCISSORS" && computerChoice == "PAPER") {
         winCount++;
         return "YOU WIN!";
     } else if (playerChoice == "SCISSORS" && computerChoice == "ROCK") {
         lossCount++;
         return "YOU LOSE...";
+    } else if (playerChoice == "SCISSORS" && computerChoice == "LIZARD") {
+        winCount++;
+        return "YOU WIN!";
+    } else if (playerChoice == "SCISSORS" && computerChoice == "SPOCK") {
+        lossCount++;
+        return "YOU LOSE...";
+    } else if (playerChoice == "LIZARD" && computerChoice == "PAPER") {
+        winCount++;
+        return "YOU WIN!";
+    } else if (playerChoice == "LIZARD" && computerChoice == "ROCK") {
+        lossCount++;
+        return "YOU LOSE...";
+    } else if (playerChoice == "LIZARD" && computerChoice == "SCISSORS") {
+        lossCount++;
+        return "YOU LOSE...";
+    } else if (playerChoice == "LIZARD" && computerChoice == "SPOCK") {
+        winCount++;
+        return "YOU WIN!";
+    } else if (playerChoice == "SPOCK" && computerChoice == "PAPER") {
+        lossCount++;
+        return "YOU LOSE...";
+    } else if (playerChoice == "SPOCK" && computerChoice == "ROCK") {
+        winCount++;
+        return "YOU WIN!";
+    } else if (playerChoice == "SPOCK" && computerChoice == "LIZARD") {
+        lossCount++;
+        return "YOU LOSE...";
+    } else if (playerChoice == "SPOCK" && computerChoice == "SCISSORS") {
+        winCount++;
+        return "YOU WIN!";
     }
 }
 
